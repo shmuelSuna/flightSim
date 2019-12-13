1f80ca5bfac0c12db7d66650004a79384daa6db4
oripo1312 <oriporia@gmail.com>
2019-12-13 15:58:09 +0200

    lexer in process

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
+#include <fstream>
+using namespace std;
+std::vector<string> static lexer(const std::string&);
+
 
 int main() {
+    lexer("fly.txt");
 
-  return 0;
+
+    return 0;
+}
+
+
+
+std::vector<string> static lexer(const std::string& fileName) {
+    std::vector<string> lexerVector;
+
+    ifstream file(fileName);
+    if (!file.is_open()) {
+        std::cout<<"Error open the file"<<endl;
+    }
+
+
+
+    std::string line;
+    while(getline(file, line)) {
+        std::string token;
+        for (char& c : line) {
+            if (c == '(' || c == ')' || c == ' ') {
+                lexerVector.push_back(token);
+                token = "";
+                continue;
+            }
+            token.push_back(c);
+        }
+
+
+
+    }
+    return lexerVector;
+
+
 }
